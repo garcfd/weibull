a76a45d18453255f88d9fa6d1b1dfee35822ca56
garcfd <garcfd@gmail.com>
2022-11-15 19:19:36 +0000

    Add files via upload

---
+++ compile.bat
@@ -0,0 +1,2 @@
+D:\Program_Files\gfortran\bin\gfortran -static-libgfortran -m64 -fdefault-integer-8 -Wall -Wtabs -fcheck=all -fbounds-check -pedantic -fbacktrace -O2 -fopenmp -o weibull.exe weibull.f
+weibull.exe
